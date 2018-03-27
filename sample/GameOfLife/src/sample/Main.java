6ec23f5f1c11d6de414834f8ba383d779d4e2a89
Kendy Colon <kcolon018@gmail.com>
2018-03-27 03:55:44 -0400

    Added buttons and Resized roots Pane, and Primary Stage.

--- sample/GameOfLife/src/sample/Main.java
+++ sample/GameOfLife/src/sample/Main.java
@@ -2,26 +2,22 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
-
-import java.awt.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         AnchorPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Game Of Life");
-        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.setTitle("Game Of Life CS485");
+        primaryStage.setScene(new Scene(root, 800, 700));
         root.getChildren().add(CreateBoxes());
 
         primaryStage.show();
@@ -29,7 +25,7 @@ public class Main extends Application {
 
     public Pane CreateBoxes(){
         Pane roots = new Pane();
-        roots.setPrefSize(600,600);
+        roots.setPrefSize(500,500);
         for (int i = 0; i< 35; i++)
         {
             for(int j = 0; j<70; j++){
@@ -41,8 +37,8 @@ public class Main extends Application {
             }
         }
 
-        roots.setLayoutX(40);
-        roots.setLayoutY(50);
+        roots.setLayoutX(70);
+        roots.setLayoutY(90);
         return roots;
     }
 
